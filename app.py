9102542604a195f6e52ec55dd2081b3e9bfa15ed
Rajath E S <rajathes999@gmail.com>
2022-05-16 23:14:47 +0530

    Small updates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 import pickle
 
 predict_input_args = reqparse.RequestParser()
-predict_input_args.add_argument('rm', type=float, location='form')
-predict_input_args.add_argument('lstst', type=float, location='form')
-predict_input_args.add_argument('ptratio', type=float, location='form')
+predict_input_args.add_argument('rm', type=float)
+predict_input_args.add_argument('lstst', type=float)
+predict_input_args.add_argument('ptratio', type=float)
 
 
 app = Flask(__name__)
@@ -17,14 +17,19 @@ with open('model/modelfile', 'rb') as f:
 
 class Predict(Resource):
     def post(slef):
+        # print('Request json {}'.format(request.json))
         args = predict_input_args.parse_args()
         predicted_value = model.predict([[args['rm'], args['lstst'], args['ptratio']]])
-        print('Predicted Value {}'.format(predicted_value))
         return {'PredictedPrice': predicted_value[0]}
+
+class Hello(Resource):
+    def get(self):
+        return 'Hello there...'
         
 
 
 api.add_resource(Predict, '/predict')
+api.add_resource(Hello, '/')
 
 if __name__ == '__main__':
-    app.run(port=4300)
+    app.run(debug=True, port=8080, host='0.0.0.0')
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import requests
 
 data = pd.read_csv('data/test_data.csv')
 
-BASE_URI = 'http://127.0.0.1:4300/'
+BASE_URI = 'http://127.0.0.1:8080/'
 
 for i, d in data.iterrows():
     request_body = {
@@ -12,8 +12,10 @@ for i, d in data.iterrows():
         'lstst': d[1],
         'ptratio': d[2]
     }
-    result = requests.post(BASE_URI + 'predict', request_body)
     print(BASE_URI + 'predict')
+    print(request_body)
+    result = requests.post(BASE_URI + 'predict', request_body)
+    
     print('Status {}'.format(result.status_code))
     print(d[0], d[1], d[2])
     print(result.raw)
